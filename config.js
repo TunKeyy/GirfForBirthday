e68391004a3cf9039d859ec18f87eeff3167c524
Nguyễn Tuấn Kha <20521427@gm.uit.edu.vn>
2024-04-17 16:04:27 +0700

    update

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Đi';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100074337730683';
+const eventName = 'Mở';
+const titleCard = 'Happy Birthday My Sister🎂';
+const contentCard = 'Happy Birthday! Tuổi mới khỏe mạnh, học giỏi và luôn vui vẻ nhé!🎉';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'photo.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
